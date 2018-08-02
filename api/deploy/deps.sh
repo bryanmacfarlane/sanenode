3fa3cdba8d8686f001dcd6e897a33bd4d70d5842
Bryan MacFarlane <bryanmac@microsoft.com>
2018-08-02 15:08:51 -0400

    starting k8s

---
+++ api/deploy/deps.sh
@@ -0,0 +1,4 @@
+# https://gomplate.hairyhenderson.ca/installing/
+echo Installing gomplate
+curl -o /usr/local/bin/gomplate -sSL https://github.com/hairyhenderson/gomplate/releases/download/v2.7.0/gomplate_darwin-amd64-slim
+chmod 755 /usr/local/bin/gomplate
